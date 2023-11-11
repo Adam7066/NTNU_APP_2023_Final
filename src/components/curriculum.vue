5db10af6ce9f155df9af4bdeb730a5dd1c0c5f17
KutsunaSubaRya <index20010928@gmail.com>
2023-11-11 11:19:30 +0800

    style(curriculum): change title to header

--- src/components/curriculum.vue
+++ src/components/curriculum.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>課表</div>
+  <div class="w-full h-full p-4">
+    <div class="text-center text-xl font-semibold">課表</div>
+  </div>
+  <Divider/>
 </template>
+<script setup lang="ts">
+  import {
+    Divider
+  } from 'tdesign-mobile-vue'
+</script>
